feb1cadf135bd5d2429057d43aa68c10eb118fa6
adsons3c <adsonemanuels3c@gmail.com>
2018-12-12 16:58:58 -0300

    Adicao de choices

--- sce/actives_IT/models.py
+++ sce/actives_IT/models.py
@@ -10,6 +10,24 @@ class Setor(models.Model):
 
 
 class Computadores(models.Model):
+
+    memoria_choices = (
+    ('2GB', '2GB'),
+    ('3GB', '3GB'),
+    ('4GB', '4GB'),
+    ('6GB', '6GB'),
+    ('8GB', '8GB'),
+    ('16GB', '16GB'),
+    ('32GB', '32GB'),
+    )
+    hd_choices = (
+    ('240GB', '240GB'),
+    ('250GB', '250GB'),
+    ('500GB', '500GB'),
+    ('1000GB', '1000GB'),
+    ('2000GB', '2000GB'),
+    )
+
     modelo = models.CharField(max_length=100)
     tombamento = models.IntegerField()
     numero_serie = models.CharField(max_length=100)
@@ -17,6 +35,9 @@ class Computadores(models.Model):
     licenca_so = models.CharField(max_length=100)
     ip = models.GenericIPAddressField()
     mac = models.CharField(max_length=50)
+    processador = models.CharField(max_length=30)
+    memoria = models.CharField(max_length=4, choices=memoria_choices, default='4GB')
+    hd = models.CharField(max_length=10, choices=hd_choices)
     data_ultima_manutencao = models.DateField(blank=True)
     data_proxima_manutencao = models.DateField(blank=True)
     descricao_manutencao = models.TextField()
